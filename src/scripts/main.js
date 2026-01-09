2e3fa3be09689be6d4e64d86099b4af56682f0cb
Anastasiia <anastasiia.dosiak@gmail.com>
2026-01-09 18:48:12 +0200

    add solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,16 @@ const allPopulation = document.querySelectorAll('span.population');
 const populationArr = Array.from(allPopulation);
 
 const populationMap = populationArr.map((el) =>
-  el.textContent.trim().replaceAll(/\D/g, ''));
+  el.textContent.trim().replaceAll(/\D/g, ''),
+);
 
 const numbers = [];
 
 for (const p of populationMap) {
-  if (!Number.isNaN(p)) {
-    numbers.push(parseInt(p));
+  const n = Number(p);
+
+  if (!Number.isNaN(n)) {
+    numbers.push(n);
   }
 }
 
