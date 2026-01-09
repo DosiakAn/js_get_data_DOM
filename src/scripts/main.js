c6bdc99b52262b78214b4a7385598d9e7de3e87f
Anastasiia <anastasiia.dosiak@gmail.com>
2026-01-09 18:33:11 +0200

    add solutions

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write your code here
+const allPopulation = document.querySelectorAll('span.population');
+
+const populationArr = Array.from(allPopulation);
+
+const populationMap = populationArr.map((el) =>
+  el.textContent.trim().replaceAll(/\D/g, ''));
+
+const numbers = [];
+
+for (const p of populationMap) {
+  if (!Number.isNaN(p)) {
+    numbers.push(parseInt(p));
+  }
+}
+
+const totalPopulation = document.querySelector('span.total-population');
+const averagePopulation = document.querySelector('span.average-population');
+
+let total = numbers.reduce((acc, curValue) => acc + curValue, 0);
+
+let average = Math.round(total / numbers.length);
+
+total = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+average = average.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+totalPopulation.textContent = total;
+
+averagePopulation.textContent = average;
